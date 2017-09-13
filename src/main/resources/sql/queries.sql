b25373965d92b512e0efba4fa6765780d616ec16
thiago <thiagoprocaci@gmail.com>
2017-09-13 01:36:34 -0300

    query

--- src/main/resources/sql/queries.sql
+++ src/main/resources/sql/queries.sql
@@ -50,3 +50,73 @@ select * from  post p where p.post_type = 2
     )A order by A.id
 
 
+-------------------------
+
+quartiles
+
+
+select min(u.reputation) as reputation, (select 'min' from dual) as description from chemistry_user u
+union all
+
+select * from (
+	select max(B.reputation) as reputation, B.description  from (
+
+ SELECT
+	  A.rank,
+	  A.id,
+	  A.reputation,
+	  (select 'Q1' from dual) as description,
+	  CASE WHEN A.rank <= (select (count(*) * 25)/100 from chemistry_user)  THEN 'Q1_start'
+	  END AS 'quartile'
+	FROM (
+			select @rownum:=@rownum + 1 rank, p.* from chemistry_user p ,
+			(SELECT @rownum:=0) r order by reputation asc
+
+	)A
+
+	)B where B.quartile = 'Q1_start' order by B.reputation
+)C
+	union all
+select * from (
+	SELECT avg(t1.reputation) as reputation, (select 'median' from dual) as description FROM (
+SELECT @rownum:=@rownum+1 as `row_number`, d.reputation
+  FROM chemistry_user d,  (SELECT @rownum:=0) r
+  WHERE 1
+  ORDER BY d.reputation
+) as t1,
+(
+  SELECT count(*) as total_rows
+  FROM chemistry_user d
+  WHERE 1
+) as t2
+WHERE 1
+AND t1.row_number in ( floor((total_rows+1)/2), floor((total_rows+2)/2))
+	)C
+
+	union all
+
+select avg(u.reputation) as reputation, (select 'mean' from dual) as description from chemistry_user u
+union all
+
+select * from (
+select min(B.reputation) as reputation, B.description from (
+
+ SELECT
+	  A.rank,
+	  A.id,
+	  A.reputation,
+	  (select 'Q3' from dual) as description,
+	  CASE WHEN A.rank >= (select (count(*) * 75)/100 from chemistry_user) THEN 'Q3_start'
+	  END AS 'quartile'
+	FROM (
+			select @rownum:=@rownum + 1 rank, p.* from chemistry_user p ,
+			(SELECT @rownum:=0) r order by reputation asc
+
+	)A
+
+	)B where B.quartile = 'Q3_start' order by B.reputation
+)C
+
+union all
+
+select max(u.reputation) as reputation, (select 'max' from dual) as description from chemistry_user u
