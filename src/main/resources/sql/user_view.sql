e3e0cbb8f6b9aea02fe46423b5177417eb656410
thiago <thiagoprocaci@gmail.com>
2017-11-28 23:43:11 -0200

    sqls

--- src/main/resources/sql/user_view.sql
+++ src/main/resources/sql/user_view.sql
@@ -1,3 +1,8 @@
 create or replace VIEW ai_user AS select * from comm_user_ranking('ai.stackexchange.com');
 create or replace VIEW biology_user AS select * from comm_user_ranking('biology.stackexchange.com');
 create or replace VIEW chemistry_user AS select * from comm_user_ranking('chemistry.stackexchange.com');
+
+create or replace VIEW ai_user_participation_count as  select * from comm_user_participation_count('ai.stackexchange.com');
+create or replace VIEW biology_user_participation_count as  select * from comm_user_participation_count('biology.stackexchange.com');
+create or replace VIEW chemistry_user_participation_count as  select * from comm_user_participation_count('chemistry.stackexchange.com');
+
--- src/main/resources/sql/view-postgres.sql
+++ src/main/resources/sql/view-postgres.sql
@@ -1,73 +1,116 @@
 
-select
-	A.*,
+select B.*  ,
+	case when B.top_5 = 0 and B.top_10 = 1  then 1
+		 else 0
+		 end as top_5_10,
 
-	case
-		when A.ranking <= (A.total * 5/100) then 1
-		when A.ranking > (A.total * 5/100) then 0
-	end as top_5,
+	case when B.top_10 = 0 and B.top_15 = 1  then 1
+		 else 0
+		 end as top_10_15,
 
-	case
-		when A.ranking <= (A.total * 10/100) then 1
-		when A.ranking > (A.total * 10/100) then 0
-	end as top_10,
+	case when B.top_15 = 0 and B.top_20 = 1  then 1
+		 else 0
+		 end as top_15_20,
 
-	case
-		when A.ranking <= (A.total * 15/100) then 1
-		when A.ranking > (A.total * 15/100) then 0
-	end as top_15,
+	case when B.top_20 = 0 and B.top_25 = 1  then 1
+		 else 0
+		 end as top_20_25,
 
-	case
-		when A.ranking <= (A.total * 20/100) then 1
-		when A.ranking > (A.total * 20/100) then 0
-	end as top_20,
+	case when B.top_25 = 0 and B.top_30 = 1  then 1
+		 else 0
+		 end as top_25_30,
 
-	case
-		when A.ranking <= (A.total * 25/100) then 1
-		when A.ranking > (A.total * 25/100) then 0
-	end as top_25,
+	case when B.top_30 = 0 and B.top_35 = 1  then 1
+		 else 0
+		 end as top_30_35,
 
-	case
-		when A.ranking <= (A.total * 30/100) then 1
-		when A.ranking > (A.total * 30/100) then 0
-	end as top_30,
+	case when B.top_35 = 0 and B.top_40 = 1  then 1
+		 else 0
+		 end as top_35_40,
 
-	case
-		when A.ranking <= (A.total * 35/100) then 1
-		when A.ranking > (A.total * 35/100) then 0
-	end as top_35,
+	case when B.top_40 = 0 and B.top_45 = 1  then 1
+		 else 0
+		 end as top_40_45,
 
-	case
-		when A.ranking <= (A.total * 40/100) then 1
-		when A.ranking > (A.total * 40/100) then 0
-	end as top_40,
+	case when B.top_45 = 0 and B.top_50 = 1  then 1
+		 else 0
+		 end as top_45_50,
 
-	case
-		when A.ranking <= (A.total * 45/100) then 1
-		when A.ranking > (A.total * 45/100) then 0
-	end as top_45,
+	case when B.top_50 = 0  then 1
+		 else 0
+		 end as top_50_100
+from (
 
-	case
-		when A.ranking <= (A.total * 50/100) then 1
-		when A.ranking > (A.total * 50/100) then 0
-	end as top_50
+	select
+		A.*,
 
-from
+		case
+			when A.ranking <= (A.total * 5/100) then 1
+			when A.ranking > (A.total * 5/100) then 0
+		end as top_5,
+
+		case
+			when A.ranking <= (A.total * 10/100) then 1
+			when A.ranking > (A.total * 10/100) then 0
+		end as top_10,
+
+		case
+			when A.ranking <= (A.total * 15/100) then 1
+			when A.ranking > (A.total * 15/100) then 0
+		end as top_15,
+
+		case
+			when A.ranking <= (A.total * 20/100) then 1
+			when A.ranking > (A.total * 20/100) then 0
+		end as top_20,
+
+		case
+			when A.ranking <= (A.total * 25/100) then 1
+			when A.ranking > (A.total * 25/100) then 0
+		end as top_25,
+
+		case
+			when A.ranking <= (A.total * 30/100) then 1
+			when A.ranking > (A.total * 30/100) then 0
+		end as top_30,
+
+		case
+			when A.ranking <= (A.total * 35/100) then 1
+			when A.ranking > (A.total * 35/100) then 0
+		end as top_35,
+
+		case
+			when A.ranking <= (A.total * 40/100) then 1
+			when A.ranking > (A.total * 40/100) then 0
+		end as top_40,
+
+		case
+			when A.ranking <= (A.total * 45/100) then 1
+			when A.ranking > (A.total * 45/100) then 0
+		end as top_45,
+
+		case
+			when A.ranking <= (A.total * 50/100) then 1
+			when A.ranking > (A.total * 50/100) then 0
+		end as top_50
+
+	from
 
 
-(
-	SELECT u.id,
-		u.reputation,
-	    row_number() OVER (
-	            order by u.reputation desc
-	    ) as ranking,
-	    (select count(*) from comm_user
-	    where id_community
-	    in (select co.id from community co where co.name = 'ai.stackexchange.com')) as total
-	  FROM comm_user u
-	  where u.id_community in (select co.id from community co where co.name = 'ai.stackexchange.com')
-	  order by u.reputation desc
-)A
+	(
+		SELECT
+			u.*,
+		    row_number() OVER (
+		            order by u.reputation desc
+		    ) as ranking,
+		    (select count(*) from comm_user
+		    where id_community
+		    in (select co.id from community co where co.name = 'ai.stackexchange.com')) as total
+		  FROM comm_user u
+		  where u.id_community in (select co.id from community co where co.name = 'ai.stackexchange.com')
+		  order by u.reputation desc
+	)A
+)B
 
 
 
@@ -115,3 +158,5 @@ select ut.id as user_id, ut.reputation,
 	--order by ut.reputation asc
 
 
+
+-- select column_name, data_type, character_maximum_length from INFORMATION_SCHEMA.COLUMNS where table_name = 'comm_user';
