6d286d8e1e77da86081031321f65a1bc573fb126
thiago <thiagoprocaci@gmail.com>
2018-01-09 00:16:54 -0200

    sqls

--- src/main/resources/sql/tag_layer-func.sql
+++ src/main/resources/sql/tag_layer-func.sql
@@ -1,18 +1,18 @@
 CREATE OR REPLACE FUNCTION public.tag_layer(community_name text)
  RETURNS TABLE(tag text,
+ 			"count" bigint,
  			 pos bigint,
  			 layer text
  )
  LANGUAGE sql
 AS $function$
-
 with TAGS as  (
 	select
 	id,
 	tag1,
 	tag2,
 	tag3
-	from communities.post_tags($1)
+	from post_tags($1)
 ),
 IMPORTANCE as (
 	select count(*), A.tag from (
@@ -34,20 +34,19 @@ IMPORTANCE as (
 	group by A.tag order by count(*) desc
 ),
 RANKING as (
-	select tag, rank() over (order by "count" desc) as pos  from IMPORTANCE
+	select tag, "count", rank() over (order by "count" desc) as pos  from IMPORTANCE
 )
 
 
 
 select *,
 	case
-		when pos <= ((select count(*) from RANKING)/3) then '1'
-		when pos > ((select count(*) from RANKING)/3) and pos <= ((select count(*) from RANKING) * 2/3)  then '2'
-		else '3'
+		when "count" >= 1000 then '1'
+		when "count" >= 500 and "count" < 1000 then '2'
+		when "count" >= 100 and "count" < 500 then '3'
+		else '4'
 	end as layer
 
-from RANKING
+from RANKING order by pos asc
 
 $function$
-
-
